4fea60836867fb3cdf13717e439d1b5df4fdbc60
Alex Cook <alexmcook.github@gmail.com>
2017-11-20 17:53:42 -0700

    Fix essenceText data

--- essenceTexts.py
+++ essenceTexts.py
@@ -24,13 +24,23 @@ data = []
 
 if (not os.path.exists('./src/essence_data.json')):
   for essence in essences:
-    url = 'https://pathofexile.gamepedia.com/api.php?action=browsebysubject&format=json&subject=' + tier[essence['tier']] + '%20Essence%20of%20' + essence['name']
+    url = 'https://pathofexile.gamepedia.com/api.php?action=browsebysubject&format=json&subject='
+    if (tier[essence['tier']] != ''):
+      url += tier[essence['tier']] + '%20'
+    url += 'Essence%20of%20' + essence['name']
     r = requests.get(url)
     if (r.status_code == 200):
-      print('OK: ' + str(r.status_code) + ' ' + tier[essence['tier']] + ' Essence of ' + essence['name'])
-      data.append(json.loads(r.text))
+      essenceID = ' '
+      if (tier[essence['tier']] != ''):
+        essenceID += tier[essence['tier']] + ' '
+      essenceID += 'Essence of ' + essence['name']
+      print('OK: ' + str(r.status_code) + essenceID)
+      data.append(OrderedDict([
+        ('id', essence['name'] + str(essence['tier'])),
+        ('json', json.loads(r.text))
+        ]))
     else:
-      print('ERR: ' + str(r.status_code) + ' ' + tier[essence['tier']] + ' Essence of ' + essence['name'])
+      print('ERR: ' + str(r.status_code) + essenceID)
 
   with open('./src/essence_data.json', 'w+') as out:
     json.dump(data, out)
@@ -40,12 +50,16 @@ essenceData = json.loads(essenceData_data)
 
 output = []
 for essence in essenceData:
-  rawText = essence['query']['data'][3]['dataitem'][0]['item']
+  rawText = essence['json']['query']['data'][3]['dataitem'][0]['item']
   essenceText = re.compile('<br ?/?>').split(rawText)
   trimmedText = []
   for text in essenceText:
     trimmedText.append(text.strip())
-  output.append(trimmedText)
+  output.append({ text: trimmedText})
+  out = OrderedDict([
+      ('id', essence['id']),
+      ('text', trimmedText),
+    ])
 
 with open('./out/essenceText.json', 'w+') as out:
   json.dump(output, out)
